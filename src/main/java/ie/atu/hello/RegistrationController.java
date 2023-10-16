21a0f0d9cc0ae4eb01353b2014841f2bc2d07002
Stephen Harney <g00367573@atu.ie>
2023-10-16 22:01:08 +0100

    Links not woorking

--- src/main/java/ie/atu/hello/RegistrationController.java
+++ src/main/java/ie/atu/hello/RegistrationController.java
@@ -1,6 +1,17 @@
 package ie.atu.hello;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RegistrationController {
+
+    @PostMapping("/confirm")//endpoint confirm
+    public String confirmDetails(@RequestBody UserDetails userDetails){
+        String confirmationMessage = String.format("Received details for %s with email %s", userDetails.getName(),userDetails.getEmail());
+        return confirmationMessage;
+    }
 
 
 
